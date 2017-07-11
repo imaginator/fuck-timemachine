e71c0561a86e8435059f4bba937bfb125c13f4b6
Simon Tennant <simon@imaginator.com>
2017-07-11 12:00:02 +0200

    create launctl, run backup. Fuck TimeMachine.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fuck-timemachine
 rsync script and launchd setup to backup a laptop on home network.
+
+After dealing with failing TimeMachine backups on AFS and SMB, it's back to good old rsync
+
+Adjust paths to suit
+
+# needs
+ssh keys setup
---
+++ home-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# setup recurring with
+# launchctl bootstrap gui/$UID home-backup.plist
+
+
+# -X needed for OS X package files/directories
+
+rsync \
+  --archive \
+  --xattrs \
+  --delete \
+  --one-file-system \
+  --inplace \
+  --exclude-from="$HOME/Documents/src/fuck-timemachine/home-backup.ignorelist" \
+  /Users/simontennant simon@bunker.imaginator.com:/srv/backups/remote-servers/macbook
+
+touch /tmp/home-backup.date
