43fa400b2e11b75a30e6e4c5a3dcb70b1fb36068
Simon Tennant <simon@imaginator.com>
2017-09-08 11:24:27 +0200

    backup over the internet pipes

--- home-backup.sh
+++ home-backup.sh
@@ -3,16 +3,26 @@
 # setup recurring with
 # launchctl bootstrap gui/$UID home-backup.plist
 
-
 # -X needed for OS X package files/directories
 
 rsync \
-  --archive \
-  --xattrs \
-  --delete \
+  -e 'ssh -p 23' \
+  --compress \
+  --numeric-ids \
+  --links \
+  --hard-links \
   --one-file-system \
+  --itemize-changes \
+  --times \
+  --recursive \
+  --perms \
+  --owner \
+  --group \
+  --stats \
+  --human-readable \
+  --delete \
   --inplace \
   --exclude-from="$HOME/Documents/src/fuck-timemachine/home-backup.ignorelist" \
-  /Users/simontennant simon@bunker.imagilan:/srv/backups/remote-servers/macbook
+  /Users/simontennant simon@bunker.imaginator.com:/srv/backups/remote-servers/macbook
 
 touch /tmp/home-backup.date
